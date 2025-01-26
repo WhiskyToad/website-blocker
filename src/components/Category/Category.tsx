3e28c94b55acdc1e2539b6b8998c1a28b1108d99
Steven <stevencraig321@gmail.com>
2025-01-26 17:04:51 +0000

    feat: added add category button

--- src/components/Category/Category.tsx
+++ src/components/Category/Category.tsx
@@ -2,6 +2,7 @@ import type { CategoryHeaderProps } from '../CategoryHeader/CategoryHeader';
 import CategoryHeader from '../CategoryHeader/CategoryHeader';
 import type { CategorySiteListProps } from '../CategorySiteList/CategorySiteList';
 import CategorySiteList from '../CategorySiteList/CategorySiteList';
+import { FaPlus } from 'react-icons/fa';
 
 export interface CategoryProps {
   categories: {
@@ -13,7 +14,13 @@ export interface CategoryProps {
 const Category = ({ categories }: CategoryProps) => {
   return (
     <div className="p4 space-y-4">
-      <h2 className="text-3xl text-center">Categories</h2>
+      <div className="flex flex-row justify-between items-center">
+        <h2 className="text-3xl text-center px-4">Categories</h2>
+        <button className="btn btn-secondary">
+          <FaPlus />
+          Add Category
+        </button>
+      </div>
       <div className="">
         {categories.map((category, index) => (
           <div
