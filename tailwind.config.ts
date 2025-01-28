5b867faf67be014d5ce8e6ffab7e1ea667d850df
Steven <stevencraig321@gmail.com>
2025-01-28 18:06:51 +0000

    feat: tweaked theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,21 @@
 export default {
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#2563eb',
+          secondary: '#f97316',
+          accent: '#10b981',
+          neutral: '#f3f4f6',
+          'base-100': '#ffffff',
+          info: '#3b82f6',
+          success: '#16a34a',
+          warning: '#facc15',
+          error: '#dc2626',
+        },
+      },
+    ],
+  },
   content: ['./src/**/*.html', './src/**/*.tsx'],
   plugins: [require('daisyui')],
 };
