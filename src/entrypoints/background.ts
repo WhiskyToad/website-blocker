6eb3302c55a8321cc8b11448bffc1194588d3a91
Steven <stevencraig321@gmail.com>
2025-02-28 10:08:47 +0000

    fix: doesnt go to sleep

--- src/entrypoints/background.ts
+++ src/entrypoints/background.ts
@@ -17,3 +17,21 @@ export default defineBackground(() => {
   //@ts-expect-error - cba typing
   browser.runtime.onMessage.addListener(handleFirefoxBlockedSitesMessage);
 });
+
+browser.alarms.create('keep-loaded-alarm-0', {
+  periodInMinutes: 1,
+});
+setTimeout(
+  () =>
+    browser.alarms.create('keep-loaded-alarm-1', {
+      periodInMinutes: 1,
+    }),
+  20000
+);
+setTimeout(
+  () =>
+    browser.alarms.create('keep-loaded-alarm-2', {
+      periodInMinutes: 1,
+    }),
+  40000
+);
